452e7b2e834149133cd92e2dd809d1afdc0641b9
andrenaMatthiasZ <matthias.zeleny@andrena.de>
2017-07-13 15:49:21 +0200

    Write Main method

--- NASATime/NASATime/Program.cs
+++ NASATime/NASATime/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace NASATime
+﻿using System;
+
+namespace NASATime
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Timer timer = new Timer();
+
+            Console.WriteLine("Bitte geben sie den Startwert des Countdowns ein.");
+            timer.setStartingValue(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Countdown: " + timer.countDown());
         }
     }
 }
