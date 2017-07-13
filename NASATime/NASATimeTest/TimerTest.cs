ffe5d875cc213943a94e25d6f8b226a9d18c5126
andrenaMatthiasZ <matthias.zeleny@andrena.de>
2017-07-13 14:15:09 +0200

    Create test method and method

---
+++ NASATime/NASATimeTest/TimerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NASATime;
+
+namespace NASATimeTest
+{
+    [TestClass]
+    public class TimerTest
+    {
+        [TestMethod]
+        public void DefaultReturnsTenToZero()
+        {
+            Timer timer = new Timer();
+
+            Assert.AreEqual("10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0", timer.countDown());
+        }
+    }
+}
