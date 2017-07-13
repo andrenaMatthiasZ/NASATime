f090d3a617fbe43af2bc1afb00c83418425ca763
andrenaMatthiasZ <matthias.zeleny@andrena.de>
2017-07-13 16:29:31 +0200

    Write AddWithCommaIfFirstStringIsNonTrivial method

--- NASATime/NASATime/Util/StringExtension.cs
+++ NASATime/NASATime/Util/StringExtension.cs
@@ -8,6 +8,20 @@ namespace NASATime.Util
 {
     public static class StringExtension
     {
+        public static String AddWithCommaIfFirstStringIsNonTrivial( this String firstString ,String secondString)
+        {
+            String combinedString = firstString;
 
+            if (firstString.Equals(null) || firstString.Equals(""))
+            {
+                combinedString = secondString;
+            }
+            else
+            {
+                combinedString = firstString + ", ";
+            }
+
+            return combinedString;
+        }
     }
 }
